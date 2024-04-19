d0f9ad8833dcb270dd5b4c6bd7e2b753d530170c
RafaelVolkmer <Rafael.V.Volkmer@gmail.com>
2024-04-18 22:09:55 -0300

    Update CriarVetorAngulos.cpp

--- CriarVetorAngulos.cpp
+++ CriarVetorAngulos.cpp
@@ -5,33 +5,28 @@
 #define TAMANHO 42
 
 static uint16_t Val_Angulos[TAMANHO] = {[ 0 ... ( TAMANHO-1 ) ] = 0};
-static uint16_t Val_ARR[TAMANHO] = {[ 0 ... ( TAMANHO-1 )] = 0};
+static uint16_t Val_ARR[TAMANHO] = {[ 0 ... ( TAMANHO-1 ) ] = 0};
 
 void Preenche_Angulo ( )
 {
-
     for (uint16_t i = 0; i < TAMANHO; i++)
     {
        Val_Angulos[i] = i + 1;
     }
-
 }
 
 void Calcula_ARR ( )
 {
-
     for(uint16_t i = 0; i < TAMANHO; i++)
     {
         uint16_t X_arr = ((ARR*Val_Angulos[i])/360);
 
         Val_ARR[i] = X_arr;
     }
-
 }
 
 int main (void)
 {
-
     Preenche_Angulo ( );
 
     Calcula_ARR ( );
@@ -44,18 +39,14 @@ int main (void)
 
         for (uint16_t i = 0; i < TAMANHO; i++)
         {
-
             arquivo << "    " << Val_ARR[i];
 
             if (i < (TAMANHO - 1) )
             {
-
                 arquivo << ",";
-
             }
 
             arquivo << std::endl;
-
         }
 
         arquivo << "};" << std::endl;
@@ -63,14 +54,11 @@ int main (void)
         arquivo.close();
 
         std::cout << "Arquivo gravado." << std::endl;
-
     } 
     else
     {
         std::cout << "Erro ao abrir o arquivo." << std::endl;
 
         return 1;
-
     }
-
 }
